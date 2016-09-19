9bfae04bb77050d22e874668b6b28ab57b0baa2e
Marios Trivyzas <marios@crate.io>
2016-09-19 11:39:14 +0200

    WIP: UnionPhase

--- sql/src/test/java/io/crate/executor/transport/TransportExecutorTest.java
+++ sql/src/test/java/io/crate/executor/transport/TransportExecutorTest.java
@@ -22,18 +22,28 @@
 package io.crate.executor.transport;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import io.crate.analyze.WhereClause;
 import io.crate.analyze.symbol.DynamicReference;
 import io.crate.analyze.symbol.Symbol;
 import io.crate.metadata.ReferenceIdent;
 import io.crate.metadata.RowGranularity;
 import io.crate.metadata.StmtCtx;
 import io.crate.metadata.TableIdent;
+import io.crate.metadata.table.TableInfo;
 import io.crate.planner.Plan;
 import io.crate.planner.Planner;
+import io.crate.planner.distribution.DistributionInfo;
+import io.crate.planner.node.dql.*;
 import io.crate.planner.node.management.KillPlan;
+import io.crate.planner.projection.Projection;
 import io.crate.testing.CollectingRowReceiver;
+import org.elasticsearch.cluster.routing.Preference;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -87,6 +97,75 @@ public class TransportExecutorTest extends BaseTransportExecutorTest {
         KillPlan plan = new KillPlan(UUID.randomUUID());
         executor.execute(plan, downstream);
         downstream.resultFuture().get(5, TimeUnit.SECONDS);
+    }
+
+    @Test
+//    @ClusterScope()
+    public void testUnionPlan() throws Exception {
+        setup.setUpCharacters();
+
+        Collection<Plan> childNodes = new ArrayList<>();
+        Planner.Context plannerContext = newPlannerContext();
+
+        TableInfo tableInfo = docSchemaInfo.getTableInfo("characters");
+        assert tableInfo != null;
+//        Reference uidReference = new Reference(
+//            new ReferenceIdent(tableInfo.ident(), "_uid"), RowGranularity.DOC, DataTypes.STRING);
+
+//        int executionNodeId = plannerContext.nextExecutionPhaseId();
+
+        // 1st collect and merge nodes
+        RoutedCollectPhase collectPhase1 = new RoutedCollectPhase(
+            plannerContext.jobId(),
+            plannerContext.nextExecutionPhaseId(),
+            "collect1",
+            plannerContext.allocateRouting(tableInfo, WhereClause.MATCH_ALL, Preference.PRIMARY.type()),
+            RowGranularity.DOC,
+            ImmutableList.<Symbol>of(),
+            ImmutableList.<Projection>of(),
+            WhereClause.MATCH_ALL,
+            DistributionInfo.DEFAULT_MODULO
+        );
+        MergePhase mergeNode1 = MergePhase.localMerge(
+            plannerContext.jobId(),
+            plannerContext.nextExecutionPhaseId(),
+            ImmutableList.<Projection>of(),
+            collectPhase1.executionNodes().size(),
+            collectPhase1.outputTypes());
+        childNodes.add(new CollectAndMerge(collectPhase1, mergeNode1));
+
+        // 2nd collect and merge nodes
+        RoutedCollectPhase collectPhase2 = new RoutedCollectPhase(
+            plannerContext.jobId(),
+            plannerContext.nextExecutionPhaseId(),
+            "collect2",
+            plannerContext.allocateRouting(tableInfo, WhereClause.MATCH_ALL, Preference.PRIMARY.type()),
+            RowGranularity.DOC,
+            ImmutableList.<Symbol>of(),
+            ImmutableList.<Projection>of(),
+            WhereClause.MATCH_ALL,
+            DistributionInfo.DEFAULT_MODULO
+        );
+        MergePhase mergeNode2 = MergePhase.localMerge(
+            plannerContext.jobId(),
+            plannerContext.nextExecutionPhaseId(),
+            ImmutableList.<Projection>of(),
+            collectPhase2.executionNodes().size(),
+            collectPhase2.outputTypes());
+        childNodes.add(new CollectAndMerge(collectPhase2, mergeNode2));
+
+        UnionPhase unionPhase = new UnionPhase(plannerContext.jobId(),
+                                               plannerContext.nextExecutionPhaseId(),
+//                                                executionNodeId,
+                                               "union",
+                                               ImmutableList.<Projection>of(),
+                                               Arrays.asList(mergeNode1, mergeNode2),
+                                               ImmutableSet.<String>of());
+
+        UnionPlan unionPlan = new UnionPlan(unionPhase, childNodes);
+        CollectingRowReceiver rowReceiver = new CollectingRowReceiver();
+        executor.execute(unionPlan, rowReceiver);
+        assertThat(rowReceiver.result().size(), is(2));
     }
 
     protected Planner.Context newPlannerContext() {
